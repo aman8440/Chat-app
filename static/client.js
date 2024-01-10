e4a1cb7f39dfefc93198bcb26579ce9c69c9aaf6
Aman Gupta <amang4885@gmail.com>
2024-01-10 21:54:31 +0530

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,5 +1,5 @@
 const  src="/socket.io/socket.io.js";
-const socket = io(process.env.PORT || "http://localhost:5000");
+const socket = io("http://localhost:5000");
 
 // Get DOM elements in respective Js variables
 const form = document.getElementById('send-container');
