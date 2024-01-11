8e30a6de1420fcc4b43a36af245da74580a097d4
Aman Gupta <amang4885@gmail.com>
2024-01-11 23:34:24 +0530

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,5 +1,5 @@
 const  src="/socket.io/socket.io.js";
-const socket = io("http://localhost:5000");
+const socket = io('https://chatting-website-tmee.onrender.com');
 
 // Get DOM elements in respective Js variables
 const form = document.getElementById('send-container');
