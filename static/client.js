82c7fdda5e17b947f4c014ac7985a89e937fde60
Aman Gupta <amang4885@gmail.com>
2024-01-12 10:09:45 +0530

    Update client.js

--- static/client.js
+++ static/client.js
@@ -21,7 +21,7 @@ const addEntry = ({ user, message }, you) => {
     dom.feed.appendChild(entry);
 };
 // Audio that will play on receiving messages
-var audio = new Audio('./tune.mp3');
+var audio = new Audio('tune.mp3');
 
 // Function which will append event info to the container
 const append = (message, position)=>{
@@ -31,6 +31,7 @@ const append = (message, position)=>{
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
     if(position =='left'){ 
+        audio.volume = 0.2;
         audio.play();
     }
 }
